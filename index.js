a65589cbe05d9dbd4c08ca00f19abbb8310706a0
Adam Rudd <adam.rudd@uqconnect.edu.au>
2014-08-04 15:11:50 +0200

    Remove #{min,max}Price

--- index.js
+++ index.js
@@ -48,23 +48,11 @@ Zanox.prototype.program = function (program) {
   return this._programs.push(program), this;
 };
 
-Zanox.prototype.minPrice = function (min) {
-  return this._minPrice = min, this;
-};
-
-Zanox.prototype.maxPrice = function (max) {
-  return this._maxPrice = max, this;
-};
-
 Zanox.prototype.price = function (price) {
-  if (_.isArray(price)) {
-    this._minPrice = price[0], this._maxPrice = price[1];
-  } else if (_.isString(price)) {
-    var split = price.split('..');
+  price = ('string' === typeof price) ? price.split('..') : price;
 
-    if (split[0]) this._minPrice = split[0];
-    if (split[1]) this._maxPrice = split[1];
-  }
+  if (price[0]) this._minPrice = price[0];
+  if (price[1]) this._maxPrice = price[1];
 
   return this;
 };
