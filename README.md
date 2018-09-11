5f1e612c4726c60055fa85b4bab80b5c4c3b2770
ray <ray.g@gmx.de>
2018-09-11 18:03:20 +0200

    Use ES6 and allow searching for EAN

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Introduction
 
-This module is a simple wrapper around the zanox product search API. See [the documentation](https://developer.zanox.com/publisher-api-2011/get-products) for more information 
+This module is a simple wrapper around the zanox product search API. See [the documentation](https://developer.zanox.com/publisher-api-2011/get-products) for more information
 
 ## Installation
 
@@ -13,15 +13,34 @@ This module is a simple wrapper around the zanox product search API. See [the do
 Full example
 
 ```javascript
-var zanox = require('zanox-lookup');
+const zanox = require('zanox-lookup');
 
-zanox({keywords: 'test'})
+zanox({ keywords: 'test'} )
   .id('<zanox api key>')
   .country('DE')
   .program('33223')
   .price('10..100')
   .searchType('phrase')
   .done(function (err, results) {})
+```
+
+### New in version 1.0
+
+- All settings can be added in the constructor
+- `.done()` is a promise by default but can also return a callback like before
+- Instead of using keywords the EAN can be passed into the request
+
+Example
+
+```javascript
+const zanox = require('zanox-lookup');
+
+zanox({ id: '<zanox api key>', programs: '1234,9876', ean: '98264839' })
+  .program('19283') // add another SINGLE program for whatever reason
+  .one(true) // returns one product/object instead of an array of products
+  .done()
+  .then(res => console.log(res))
+  .catch(err => { throw err })
 ```
 
 ## License
--- index.js
+++ index.js
@@ -1,169 +1,217 @@
-/**
- * Module dependencies
- */
-var request = require('superagent')
-  , accounting = require('accounting')
-  , currency = require('currency-symbol-map')
-  , _ = require('underscore');
+const axios = require('axios')
+const _ = require('underscore')
 
-var endpoint = 'https://api.zanox.com/json/2011-03-01/products';
+const endpoint = 'https://api.zanox.com/json/2011-03-01/products'
 
-module.exports = function (opts) {
-  return new Zanox(opts);
-};
+class Zanox {
+  constructor (opts) {
+    if (typeof opts === 'string') {
+      this._keywords = opts
+      return
+    }
 
-var Zanox = function Zanox (opts) {
-  if ('object' === typeof opts) {
-    this._keywords = opts.keywords;
-  } else {
-    this._keywords = opts;
+    const {
+      id, keywords, country, category, programs,
+      price, page, exactMatch, searchType, limit, ean
+    } = opts
+
+    this._partership = 'confirmed'
+    this._searchType = searchType || 'phrase'
+
+    if (id) this._id = id
+    if (keywords) this._keywords = keywords
+    if (country) this._country = country
+    if (category) this._category = category
+    if (programs) {
+      this._programs = programs.split(',')
+    }
+    if (price) {
+      if (typeof price === 'string') price = price.split('..')
+
+      if (price[0]) this._minPrice = price[0]
+      if (price[1]) this._maxPrice = price[1]
+    }
+    if (page) this._page = page
+    if (exactMatch) this._exactMatch = exactMatch
+    if (limit) this._limit = limit
+    if (ean) this._ean = ean
   }
 
-  // Setup defaults
-  _.defaults(this, {
-    _partnership: 'confirmed'
-  });
-};
+  id (id) {
+    this._id = id
+    return this
+  }
 
-Zanox.prototype.id = function (id) {
-  return this._id = id, this;
-};
+  keywords (keywords) {
+    this._keywords = keywords
+    return this
+  }
 
-Zanox.prototype.keywords = function (kw) {
-  return this._keywords = kw, this;
-};
+  country (country) {
+    this._country = country
+    return this
+  }
 
-Zanox.prototype.country = function (country) {
-  return this._country = country, this;
-};
+  category (category) {
+    this._category = category
+    return this
+  }
 
-Zanox.prototype.category = function (category) {
-  return this._category = category, this;
-};
+  program (program) {
+    if (!this._programs) this._programs = []
+    this._programs.push(program)
+    return this
+  }
 
-Zanox.prototype.program = function (program) {
-  if (!this._programs) this._programs = [];
+  price (price) {
+    if (typeof price === 'string') price = price.split('..')
 
-  return this._programs.push(program), this;
-};
+    if (price[0]) this._minPrice = price[0]
+    if (price[1]) this._maxPrice = price[1]
+    return this
+  }
 
-Zanox.prototype.price = function (price) {
-  price = ('string' === typeof price) ? price.split('..') : price;
+  page (page) {
+    this._page = page
+    return this
+  }
 
-  if (price[0]) this._minPrice = price[0];
-  if (price[1]) this._maxPrice = price[1];
+  one (one) {
+    this._one = one
+    return this
+  }
 
-  return this;
-};
+  exactMatch (exactMatch) {
+    this._exactMatch = exactMatch
+    return this
+  }
 
-Zanox.prototype.page = function (page) {
-  if ('undefined' === typeof page) return this;
-  return this._page = page, this;
-};
+  searchType (searchType) {
+    if (!searchType) {
+      this._searchType = 'phrase'
+    } else if (searchType !== 'contextual' && searchType !== 'phrase') {
+      throw new Error('Invalid search type')
+    }
+    return this
+  }
 
-Zanox.prototype.one = function (one) {
-  one = ('undefined' === typeof one) ? true : !!one;
-  return this._one = one, this;
-};
+  limit (limit) {
+    this._limit = limit
+    return this
+  }
 
-Zanox.prototype.exactMatch = function (exactMatch) {
-  if (!exactMatch) return this;
-  return this._exactMatch = exactMatch, this;
-  //exactMatch = ('undefined' === typeof exactMatch) ? true : !!exactMatch;
-  //return this._exactMatch = exactMatch, this;
-};
+  ean (ean) {
+    this._ean = ean
+    return this
+  }
 
-Zanox.prototype.searchType = function (t) {
-  if ('contextual' !== t && 'phrase' !== t) throw new Error('Invalid search type');
-  return this._searchType = t, this;
-};
+  makeRequest () {
+    return new Promise((resolve, reject) => {
+      const query = { partnership: this._partnership }
 
-Zanox.prototype.limit = function (limit) {
-  if (!limit) return this;
-  return this._limit = limit, this;
-};
+      if (this._keywords) query.q = this._keywords
+      if (this._ean) query.ean = this._ean
+      if (this._id) query.connectid = this._id
+      if (this._searchType) query.searchtype = this._searchType
+      if (this._country) query.region = this._country
+      if (this._category) query.merchantcategory = this._category
+      if (this._minPrice) query.minprice = this._minPrice
+      if (this._maxPrice) query.maxprice = this._maxPrice
+      if (this._programs) query.programs = this._programs.join(',')
+      if (this._page) query.page = this._page
 
-Zanox.prototype.done = function (cb) {
-  return request
-    .get(endpoint)
-    .query({searchtype: this._searchType})
-    .query({connectid: this._id})
-    .query({q: this._keywords})
-    .query({region: this._country})
-    .query({merchantcategory: this._category})
-    .query({partnership: this._partnership})
-    .query({minprice: this._minPrice})
-    .query({maxprice: this._maxPrice})
-    .query({programs: this._programs && this._programs.join(',')})
-    .query({page: this._page})
-    .end(function (err, res) {
-      // Handle connection errors
-      if (err) return cb(err);
+      axios.get(endpoint, { params: query })
+      .then(({ data }) => {
+        if (data.message) return reject(new Error(data.message))
 
-      // Grab body
-      var body = res.body;
+        let products = data.productItems
+          && data.productItems.productItem
+          || []
 
-      // Handle client errors
-      if (body.message) return cb(new Error(res.body.message));
+        products = format(products)
 
-      // Extract products
-      var products = body.productItems && body.productItems.productItem;
-      if (!products) products = [];
+        if (this._one) {
+          if (this._exactMatch && this._keywords) {
+            let exactMatch
+            const eachWord = this._keywords.split(' ')
+            let match = false
 
-      // Format results
-      products = format(products);
+            // Check every product we got
+            products.some(p => {
+              match = true
+              // Check if each keyword is in the product name
+              eachWord.forEach(word => {
+                if (!p.name) {
+                  match = false
+                  return
+                }
+                if (typeof p.name === 'string' && !p.name.includes(word)) {
+                  match = false
+                }
+              })
 
-      // Limit results
-      if (this._one) {
-        if (this._exactMatch) {
-          var a;
-          var qw = this._keywords.split(' ');
-          var matches = false;
-          products.some(function(p) {
-            matches = true;
-            qw.forEach(function(w) {
-              if (!p.name || typeof p.name === 'string' && p.name.indexOf(w) === -1)
-                matches = false;
-            });
-            if (matches) {
-              a = p;
-              return true;
-            }
-          });
-          products = a || null;
-        } else {
-          products = _.first(products) || null;
+              // Found a product with all keywods
+              if (match) {
+                exactMatch = p
+                return true
+              }
+            })
+            products = exactMatch || null
+          } else {
+            products = _.first(products) || null
+          }
+        } else if (this._limit) {
+          products = _.first(products, this._limit)
         }
-      } else if (this._limit) {
-        products = _.first(products, this._limit);
-      }
 
-      // Callback
-      return cb(null, products);
-    }.bind(this));
-};
+        return resolve(products)
+      })
+      .catch(reject)
+    })
+  }
 
-var format = function (products) {
-  return products.map(function (p) {
-    var price = p.price
-      , name = p.name
-      , link = links(p.trackingLinks);
+  done (cb) {
+    if (cb) {
+      return this.makeRequest()
+        .then(products => cb(null, products))
+        .catch(cb)
+    }
 
-    if (!price || !name || !link) return null;
+    return new Promise((resolve, reject) => {
+      return this.makeRequest()
+        .then(products => resolve(products))
+        .catch(reject)
+    })
+  }
+}
 
-    return {
-      name: name,
+function format (products) {
+  return products.map(p => {
+    const price = p.price
+    const name = p.name
+    const link = links(p.trackingLinks)
+
+    if (!price || !name || !link) return null
+
+    const product = {
+      name,
       id: p['@id'],
       listPrice: price,
       currency: p.currency,
       url: link
     }
-  }).filter(function (p) {
-    return p;
-  });
-};
 
-var links = function (links) {
-  if (!links) return null;
-  return links.trackingLink[0].ppc || null;
-};
+    if (p.ean) product.ean = p.ean
+
+    return product
+  }).filter(p => p)
+}
+
+function links (links) {
+  if (!links) return null
+  return links.trackingLink[0].ppc || null
+}
+
+module.exports = function (opts) {
+  return new Zanox(opts);
+}
